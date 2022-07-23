33e2345860ac480b8444b3e29701868fa6bbc74f
Artur-Weclawski <weclawski.artur@gmail.com>
2022-07-23 21:43:44 +0200

    Added resolution changing in SettingsScreen

--- core/src/com/game/Screens/SettingsScreen.java
+++ core/src/com/game/Screens/SettingsScreen.java
@@ -1,6 +1,7 @@
 package com.game.Screens;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Graphics;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
@@ -19,6 +20,7 @@ import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.Align;
 import com.game.Main;
+import jdk.internal.org.jline.utils.Display;
 
 import java.util.ArrayList;
 import java.util.Objects;
@@ -28,10 +30,10 @@ public class SettingsScreen implements Screen {
     private Stage stage;
     private Texture background;
     private BitmapFont font;
-    private TextureAtlas buttons;
-    private Skin images;
-    private TextButton bLeft, bRight;
-    private Table table_resolution;
+    private TextureAtlas buttons, buttons_default;
+    private Skin images, images_default;
+    private TextButton bLeft, bRight, bBack, bSave;
+    private Table table_resolution, table_default;
     private TextField resolution_field;
     private ArrayList<String> resolutions;
     private FreeTypeFontGenerator generator;
@@ -39,7 +41,6 @@ public class SettingsScreen implements Screen {
     public SettingsScreen(Main game){
         this.game = game;
         background = new Texture("background.png");
-
     }
 
     @Override
@@ -57,9 +58,13 @@ public class SettingsScreen implements Screen {
         stage = new Stage();
         Gdx.input.setInputProcessor(stage);
         buttons = new TextureAtlas("assets/buttons/buttons_settings.pack");
+        buttons_default = new TextureAtlas("assets/buttons/buttons_default.pack");
         images = new Skin(buttons);
+        images_default = new Skin(buttons_default);
         table_resolution = new Table(images);
         table_resolution.setBounds(200,800, 208,32);
+        table_default = new Table(images_default);
+        table_default.setBounds(700, 100, 500,200);
         TextButton.TextButtonStyle textButtonStyle_bLeft = new TextButton.TextButtonStyle();
         TextButton.TextButtonStyle textButtonStyle_bRight = new TextButton.TextButtonStyle();
         textButtonStyle_bLeft.up = images.getDrawable("resolutionButtonLeft");
@@ -72,12 +77,23 @@ public class SettingsScreen implements Screen {
         textButtonStyle_bRight.pressedOffsetX = 1;
         textButtonStyle_bRight.pressedOffsetY = -1;
         textButtonStyle_bRight.font = font;
+        TextButton.TextButtonStyle textButtonStyle_bBack = new TextButton.TextButtonStyle();
+        TextButton.TextButtonStyle textButtonStyle_bSave = new TextButton.TextButtonStyle();
+        textButtonStyle_bBack.up = images_default.getDrawable("defaultButton");
+        textButtonStyle_bBack.down = images_default.getDrawable("defaultButton"); //TODO add pressed buttons
+        textButtonStyle_bBack.pressedOffsetX = 1;
+        textButtonStyle_bBack.pressedOffsetY = -1;
+        textButtonStyle_bBack.font = font;
+        textButtonStyle_bSave.up = images_default.getDrawable("defaultButton");
+        textButtonStyle_bSave.down = images_default.getDrawable("defaultButton"); //TODO add pressed buttons
+        textButtonStyle_bSave.pressedOffsetX = 1;
+        textButtonStyle_bSave.pressedOffsetY = -1;
+        textButtonStyle_bSave.font = font;
         TextField.TextFieldStyle textFieldStyle = new TextField.TextFieldStyle();
         textFieldStyle.background = images.getDrawable("resolutionTextBar");
         textFieldStyle.font = font;
         textFieldStyle.fontColor = Color.WHITE;
         bLeft = new TextButton("", textButtonStyle_bLeft);
-
         bRight = new TextButton("", textButtonStyle_bRight);
         bRight.addListener(new ClickListener(){
             @Override
@@ -90,8 +106,6 @@ public class SettingsScreen implements Screen {
                 }else if (Objects.equals(resolution_field.getText(), resolutions.get(2))) {
                     resolution_field.setText(resolutions.get(0));
                 }
-
-
             }
         });
         bLeft.addListener(new ClickListener(){
@@ -107,13 +121,39 @@ public class SettingsScreen implements Screen {
                 }
             }
         });
+        bBack = new TextButton("Back", textButtonStyle_bBack);
+        bSave = new TextButton("Save", textButtonStyle_bSave);
+        bBack.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y){
+                game.setScreen(new MenuScreen(game));
+                dispose();
+            }
+        });
+        bSave.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y){
+                if(resolution_field.getText().equals(resolutions.get(0)))
+                {
+                    Gdx.graphics.setWindowedMode(1920,1000);
+                } else if (resolution_field.getText().equals(resolutions.get(2))) {
+                    Gdx.graphics.setWindowedMode(1600,900);
+                }else if (Objects.equals(resolution_field.getText(), resolutions.get(1))) {
+                    Gdx.graphics.setWindowedMode(1600,900);
+                }
+            }
+        });
         resolution_field = new TextField(resolutions.get(0),textFieldStyle);
         resolution_field.setAlignment(Align.center);
         table_resolution.add(bLeft);
         table_resolution.add(resolution_field);
         table_resolution.add(bRight);
+        table_default.add(bBack);
+        table_default.add(bSave);
         //table_resolution.debug();
+        table_default.debug();
         stage.addActor(table_resolution);
+        stage.addActor(table_default);
     }
 
     @Override
@@ -129,7 +169,7 @@ public class SettingsScreen implements Screen {
 
     @Override
     public void resize(int width, int height) {
-
+        stage.getViewport().update(width, height);
     }
 
     @Override
