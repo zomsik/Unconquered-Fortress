95f5c221974b3678ceabaa75f1b8f87ec1ed5031
Artur-Weclawski <weclawski.artur@gmail.com>
2022-09-25 18:03:35 +0200

    Added new cannon and cannon ball sprites.

--- core/src/com/game/Entity/Tower/CannonTower.java
+++ core/src/com/game/Entity/Tower/CannonTower.java
@@ -11,7 +11,7 @@ public class CannonTower extends Tower{
 
     public CannonTower(JSONObject turretLevels, Base base, int tileX, int tileY, float scale, GameScreen gameScreen)
     {
-        super(turretLevels, base, "cannonTower", "assets/game/towers/cannonTower.png", null, 64, new TextureRegion(new Texture(Gdx.files.internal("assets/game/bullets/arrow64.png"))), 64, tileX, tileY, scale, 2f, 200, 200, 20, gameScreen);
+        super(turretLevels, base, "cannonTower", "assets/game/towers/cannonTower.png", null, 64, new TextureRegion(new Texture(Gdx.files.internal("assets/game/bullets/cannonBall64.png"))), 64, tileX, tileY, scale, 2f, 200, 200, 20, gameScreen);
 
     }
 
